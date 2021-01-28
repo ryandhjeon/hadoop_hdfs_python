b36dbd0fd0005166f1a2f06089d741e51707702e
Ryan Jeon <ryandhjeon@gmail.com>
2021-01-28 11:45:08 -0500

    Add part2.py, part2.ipynb

--- part2.py
+++ part2.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python
 
-# @author rdyer
+# @author djeon
 
 from hdfs import InsecureClient
 from datetime import datetime
+import posixpath as psp
+import fs
 
 client = InsecureClient(url='http://localhost:9870', root='/')
 
@@ -34,30 +36,49 @@ def printfile(name, stats, prefix='-'):
 		datetime.fromtimestamp(stats['modificationTime'] / 1000).strftime('%Y-%m-%d %H:%M'),\
 		name)))
 
+print('Begin')
 
 # 1. Make a directory named: /activity1/
-content = client.content('/activity1')
-print(content)
-
-# client.makedirs(hdfs_path='/activity1/', permission=None)
-# client.makedirs(hdfs_path='/activity1/data/', permission=None)
-# print(client.list(hdfs_path='/'))
-# print('/activity1/data directory created')
+client.makedirs(hdfs_path='/activity1/', permission=None)
+client.makedirs(hdfs_path='/activity1/data/', permission=None)
 
 # 2. Put the file RandomText.txt into HDFS as the path: /activity1/data/RandomText.txt
-# client.upload(hdfs_path='/activity1/data/', local_path='/Workspace/cs6500_sp2021_r02_jeon/RandomText.txt')
-# print('Uploaded')
+client.upload(hdfs_path='/activity1/data/', local_path='./RandomText.txt')
 
 # 3. List the contents of the directory /activity1/data/
+print(client.list('/activity1/data'))
 
-# 4. Move the file /activity1/data/RandomText.txt to /activity1/data/NotSoRandomText.txt
+# 4. Move the HDFS file /activity1/data/RandomText.txt to /activity1/data/NotSoRandomText.txt
+client.rename('/activity1/data/RandomText.txt', '/activity1/data/NotSoRandomText.txt')
 
-# 5. Append the file RandomText.txt to the end of the file: /activity1/data/RandomText.txt
+with open('./RandomText.txt', 'r') as f:
+	for line in f:
+		temp = line
+
+# 5. Append the local file RandomText.txt to the end of the HDFS file: /activity1/data/NotSoRandomText.txt
+client.write(hdfs_path='/activity1/data/NotSoRandomText.txt', data=temp, append=True)
 
 # 6. List the disk space used by the directory /activity1/data/
+diskSpaceUsed = client.content('/activity1/data/', strict=True)
+print(diskSpaceUsed['spaceConsumed'])
 
-# 7. Put the file MoreRandomText.txt into HDFS as the path: /activity1/data/RandomText.txt
+# 7. Put the local file MoreRandomText.txt into HDFS as the path: /activity1/data/MoreRandomText.txt
+client.upload(hdfs_path='/activity1/data/', local_path='./MoreRandomText.txt')
+print(client.list('/activity1/data'))
 
 # 8. Recursively list the contents of the directory /activity1/
+fnames = client.list('/activity1')
+
+fpaths = [
+	psp.join(dpath, fname)
+	for dpath, _, fnames in client.walk('/activity1')
+	for fname in fnames
+]
+
+print(fpaths)
 
 # 9. Remove the directory /activity1/ and all files/directories underneath it
+client.delete(hdfs_path='/activity1', recursive=True)
+print(client.list('/'))
+
+print('End')
